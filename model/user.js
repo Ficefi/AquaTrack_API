34b32892b9c9717be58109a110a5d6cdac4a28eb
Ficefi <murdzamarkian54@gmail.com>
2024-05-11 20:47:22 +0300

    add gravatar for user avatars

--- model/user.js
+++ model/user.js
@@ -12,6 +12,7 @@ const userSchema = new Schema(
       required: [true, 'Email is required'],
       unique: true,
     },
+    avatarURL: String,
     token: {
       type: String,
       default: null,
